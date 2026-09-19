2e7d2c08c78175fad21f3f9db204bb507146b79a
markobinna120-art <markobinna120@gmail.com>
2026-09-19 01:52:59 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="social-task1-v5-final";
-const FILES=["./","./index.html","./app.js","./logo.png","./manifest.json"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html"))))});
